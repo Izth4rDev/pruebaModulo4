63b26bacba1f6010fb159baec9504970fb798309
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2024-06-13 15:54:30 -0400

    clase cliente servicio y algunos metodos listarClientes

--- src/main/java/servicio/ClienteServicio.java
+++ src/main/java/servicio/ClienteServicio.java
@@ -1,4 +1,24 @@
 package servicio;
+import lombok.Getter;
+import modelo.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 
 public class ClienteServicio {
+    private List<Cliente> listaClientes;
+    public ClienteServicio(){
+        this.listaClientes = new ArrayList<Cliente>();
+    }
+
+    public void listarClientes(){
+        if(!listaClientes.isEmpty()){
+            for(Cliente cliente : listaClientes){
+
+            }
+        }else{
+            System.out.println("La lista de clientes esta vacia");
+        }
+    }
 }
